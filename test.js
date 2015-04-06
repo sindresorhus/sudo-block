c6dc65370923fd126a77b3d4a6daf260aeae4586
David Eriksson <david@2good.nu>
2015-04-06 09:52:44 +0200

    Fix for https://github.com/sindresorhus/sudo-block/issues/6
    Allow running as root in Docker containers.

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 'use strict';
 var assert = require('assert');
 var sinon = require('sinon');
+var fs = require('fs');
 var sudoBlock = require('./');
 
 describe('sudo mode', function () {
 	beforeEach(function () {
 		process.getuid = function () {
 			return 0;
+		};
+
+		fs.statSync = function () {
+			throw new Error("ENOENT, no such file or directory '/.dockerinit'")
 		};
 
 		process.exit = sinon.spy();
@@ -32,6 +37,31 @@ describe('user mode', function () {
 	beforeEach(function () {
 		process.getuid = function () {
 			return 1000;
+		};
+
+		fs.statSync = function () {
+			throw new Error("ENOENT, no such file or directory '/.dockerinit'")
+		};
+
+		process.exit = sinon.spy();
+		console.error = sinon.spy();
+	});
+
+	it('should not prevent users', function () {
+		sudoBlock();
+		assert(!process.exit.called);
+		assert(!console.error.called);
+	});
+});
+
+describe('docker mode', function () {
+	beforeEach(function () {
+		process.getuid = function () {
+			return 0;
+		};
+
+		fs.statSync = function () {
+			return {};
 		};
 
 		process.exit = sinon.spy();
