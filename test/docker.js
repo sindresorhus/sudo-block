4c93486e58b8f8274e173eb6842e742d06035e50
Kevin Martensson <kevinmartensson@gmail.com>
2017-09-12 00:11:56 +0200

    Use `ava` for testing

---
+++ test/docker.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import test from 'ava';
+import sinon from 'sinon';
+import importFresh from 'import-fresh';
+
+test.before(() => {
+	process.getuid = () => 0;
+	fs.statSync = sinon.stub(fs, 'statSync');
+	fs.statSync.withArgs('/.dockerenv').returns({});
+	sinon.stub(process, 'exit');
+	sinon.stub(console, 'error');
+});
+
+test.after(() => {
+	fs.statSync.restore();
+	process.exit.restore();
+	console.error.restore();
+});
+
+test('should not prevent docker users', t => {
+	importFresh('..')();
+	t.false(process.exit.called);
+	t.false(console.error.called);
+});
