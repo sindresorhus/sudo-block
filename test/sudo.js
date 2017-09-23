b97bd12be58552d9e67688a61fe491f70d28d950
Kevin Mårtensson <kevinmartensson@gmail.com>
2017-09-23 12:05:08 +0200

    Use `ava` for testing (#10)

---
+++ test/sudo.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import test from 'ava';
+import sinon from 'sinon';
+import importFresh from 'import-fresh';
+
+test.before(() => {
+	process.getuid = () => 0;
+	fs.statSync = sinon.stub(fs, 'statSync');
+	fs.statSync.withArgs('/.dockerenv').throws('ENOENT, no such file or directory \'/.dockerenv\'');
+	sinon.stub(process, 'exit');
+	sinon.stub(console, 'error');
+});
+
+test.after(() => {
+	fs.statSync.restore();
+	process.exit.restore();
+	console.error.restore();
+});
+
+test('prevent sudo', t => {
+	importFresh('..')();
+	t.true(process.exit.calledOnce);
+	t.true(process.exit.calledWith(77));
+	t.true(console.error.calledOnce);
+	t.true(console.error.calledWithMatch(/You are not allowed/));
+});
+
+test('accept custom messages', t => {
+	importFresh('..')('Thou shalt not sudo!');
+	t.true(console.error.calledWithMatch(/Thou shalt not sudo!/));
+});
---
+++ test/user.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import test from 'ava';
+import sinon from 'sinon';
+import importFresh from 'import-fresh';
+
+test.before(() => {
+	process.getuid = () => 1000;
+	fs.statSync = sinon.stub(fs, 'statSync');
+	fs.statSync.withArgs('/.dockerenv').throws('ENOENT, no such file or directory \'/.dockerenv\'');
+	sinon.stub(process, 'exit');
+	sinon.stub(console, 'error');
+});
+
+test.after(() => {
+	fs.statSync.restore();
+	process.exit.restore();
+	console.error.restore();
+});
+
+test('doesn\'t prevent users', t => {
+	importFresh('..')();
+	t.false(process.exit.called);
+	t.false(console.error.called);
+});
